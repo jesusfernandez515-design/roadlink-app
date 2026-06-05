4dfe4c218afceb64f7f3ef9732cedab3cf6721f7
jesusfernandez515-design <jesusfernandez515@gmail.com>
2026-06-05 12:07:11 -0500

    Create messages page

---
+++ app/messages/page.tsx
@@ -0,0 +1,8 @@
+export default function MessagesPage() {
+  return (
+    <main style={{ padding: "40px", color: "white", background: "black", minHeight: "100vh" }}>
+      <h1>RoadLink Messages</h1>
+      <p>Inbox page is working.</p>
+    </main>
+  );
+}
