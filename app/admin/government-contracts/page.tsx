174ce5d7a1a98a782efc5627f82ff565b7e37590
jesusfernandez515-design <jesusfernandez515@gmail.com>
2026-06-21 00:53:02 -0400

    Add government contracts

---
+++ app/admin/government-contracts/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useMemo, useState } from "react";
+import { collection, doc, onSnapshot, query, setDoc } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
+
+type ContractStatus = "awarded" | "proposal" | "negotiating" | "lead
